2a050a18ac21303ae4a057f8ac64667d9513d1b0
Paritosh Singh <paritosh.singh588@gmail.com>
2019-03-18 02:01:02 +0530

    login page added

--- Mysite/network/urls.py
+++ Mysite/network/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name = 'homepage')
+    path('', views.PostList.as_view(), name = 'homepage'),
+    path('register/', views.register, name="register"),
 ]
--- Mysite/network/views.py
+++ Mysite/network/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import (
-    ListView
+    ListView,
 )
-
-posts = [
-    {
-        'author': 'Paritosh',
-        'content': 'Hello World',
-        'created_on': 'September 15, 2018',
-    },
-    {
-        'author': 'Pratyush',
-        'content': 'Hello NIBBAS',
-        'created_on': 'February 7 , 2018',
-    }
-]
-
+from .forms import UserRegistrrationForm
 
 class PostList(ListView):
     model = Post
@@ -26,5 +13,19 @@ class PostList(ListView):
 
     #template address is already passed in this
     # <app>/<model>_<viewtype>
-    #here it will be templates/network/Post_List.html
+#here it will be templates/network/Post_List.html
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            message.success(request,f'Your account have been registered!,{username}')
+
+    else:
+        form = UserRegistrrationForm()
+    return render(request=request,
+                  template_name="network/registration_view.html",
+                  context={'form': form})
 
