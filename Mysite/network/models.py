4afa8d82a0cfa0585b54c59809cb1d535c941c96
Paritosh Singh <paritosh.singh588@gmail.com>
2019-03-20 15:18:05 +0530

    Fixed the image issuee. Finally!

--- Mysite/network/models.py
+++ Mysite/network/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
-    created_on = models.TimeField()
+    created_on = models.TimeField(auto_now = True)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     image = models.ImageField(default = 'default.jpg',upload_to = 'post_pics')
 
